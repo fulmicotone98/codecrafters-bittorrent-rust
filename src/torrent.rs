e1e5fa8be744e7e9bc8732cbe86e9b672b78f30d
fulmicotone98 <jacopo.catalano98@gmail.com>
2024-10-15 16:13:42 +0200

    Split modules and contact tracker

---
+++ src/torrent.rs
@@ -0,0 +1,125 @@
+use serde::{Deserialize, Serialize};
+
+pub use hashes::Hashes;
+use sha1::{Digest, Sha1};
+
+/// Metainfo files (also known as .torrent files) are bencoded dictionaries with the following keys:
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Torrent {
+    /// The URL of the tracker.
+    pub announce: String,
+    /// This maps to a dictionary, with keys described below.
+    pub info: Info,
+}
+
+impl Torrent {
+    pub fn info_hash(&self) -> [u8; 20]{
+        // Calculate the info hash of the Torrent file
+        let info_encoded =
+            serde_bencode::to_bytes(&self.info).expect("re-encoded info section");
+        let mut hasher = Sha1::new();
+        hasher.update(info_encoded);
+        hasher.finalize().into()
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Info {
+    /// The suggested name to save the file (or directory) as. It is purely advisory.
+    ///
+    /// In single file case, the name key is the name of a file, in the multiple file case, it is the same name of a directory.
+    pub name: String,
+
+    /// The number of bytes in each piece the file is split into.
+    ///
+    /// For the purposes of transfer, files are split into fixed-size pieces which are all the same length except for possibly the last one which may be truncated. piece length is almost always a power of two, most commonly 2^18 = 256 K (BitTorrent prior to version 3.2 uses 2 20 = 1 M as default).
+    #[serde(rename = "piece length")]
+    pub plength: usize,
+
+    /// Each entry of 'pieces' is the SHA1 hash of the piece at the corresponding index.
+    pub pieces: Hashes,
+
+    #[serde(flatten)]
+    pub keys: Keys,
+}
+
+/// There is also a key 'length' or a key 'files', but not both or neither.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum Keys {
+    /// If 'length' is present then the download represents a single file.
+    SingleFile {
+        /// The length of the file in bytes.
+        length: usize,
+    },
+
+    /// Otherwise it represents a set of files which go in a directory structure.
+    ///
+    /// For the purposes of the other keys in 'Info', the multi-file case is treated as only having a single file by concatenating the files in the order they appear in the files list.
+    MultiFile {
+        /// The files list is the value files maps to, and is a list of dictionaries containing the following keys:
+        files: Vec<File>,
+    },
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct File {
+    /// The length of the file, in bytes.
+    pub length: usize,
+
+    /// Subdirectory names the file, the last is the actual file name (a zero length list is an error case).
+    pub path: Vec<String>,
+}
+
+mod hashes {
+    use serde::de::{self, Deserialize, Deserializer, Visitor};
+    use serde::ser::{Serialize, Serializer};
+    use std::fmt;
+
+    #[derive(Debug, Clone)]
+    pub struct Hashes(pub Vec<[u8; 20]>);
+    struct HashesVisitor;
+
+    impl<'de> Visitor<'de> for HashesVisitor {
+        type Value = Hashes;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a byte string whose length is a multiple of 20")
+        }
+
+        // It can't be a visit_string because the byte array is not necessarly a valid UTF-8 string.
+        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            if v.len() % 20 != 0 {
+                return Err(E::custom(format!("length is {}", v.len())));
+            }
+
+            Ok(Hashes(
+                v.chunks_exact(20)
+                    .map(|slice_20| slice_20.try_into().expect("guaranteed to be length 20"))
+                    .collect(),
+            ))
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Hashes {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            deserializer.deserialize_bytes(HashesVisitor)
+        }
+    }
+
+    impl Serialize for Hashes {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let single_slice = self.0.concat();
+            serializer.serialize_bytes(&single_slice)
+        }
+    }
+}
---
+++ src/tracker.rs
@@ -0,0 +1,105 @@
+use peers::Peers;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize)]
+pub struct TrackerRequest {
+    /// The info hash of the torrent
+    // #[serde(serialize_with = "urlencode")]
+    // pub info_hash: [u8; 20],
+
+    /// A unique identifier for your client
+    ///
+    /// A string of length 20 that you get to pick.       
+    pub peer_id: String,
+
+    /// The port your client is listening on
+    pub port: u16,
+
+    /// The total amount uploaded so far
+    pub uploaded: usize,
+
+    /// The total amount downloaded so far
+    pub downloaded: usize,
+
+    /// The number of bytes left to download
+    pub left: usize,
+
+    /// Whether the peer list should use the compact representation
+    ///
+    /// For the purposes of this challenge, set this to 1.
+    /// The compact representation is more commonly used in the wild, the non-compact representation is mostly supported for backward-compatibility.
+    pub compact: u8,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct TrackerResponse {
+    /// An integer, indicating how often your client should make a request to the tracker.
+    pub interval: usize,
+
+    /// A string, which contains list of peers that your client can connect to.
+    ///
+    /// Each peer is represented using 6 bytes. The first 4 bytes are the peer's IP address and the last 2 bytes are the peer's port number.
+    pub peers: Peers,
+}
+
+mod peers {
+    use serde::de::{self, Deserialize, Deserializer, Visitor};
+    use serde::ser::{Serialize, Serializer};
+    use std::fmt;
+    use std::net::{Ipv4Addr, SocketAddrV4};
+
+    #[derive(Debug, Clone)]
+    pub struct Peers(pub Vec<SocketAddrV4>);
+    struct PeersVisitors;
+
+    impl<'de> Visitor<'de> for PeersVisitors {
+        type Value = Peers;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("6 bytes, The first 4 bytes are the peer's IP address and the last 2 bytes are the peer's port number.")
+        }
+
+        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            if v.len() % 6 != 0 {
+                return Err(E::custom(format!("length is {}", v.len())));
+            }
+
+            Ok(Peers(
+                v.chunks_exact(6)
+                    .map(|slice_6| {
+                        SocketAddrV4::new(
+                            Ipv4Addr::new(slice_6[0], slice_6[1], slice_6[2], slice_6[3]),
+                            u16::from_ne_bytes([slice_6[4], slice_6[5]]),
+                        )
+                    })
+                    .collect(),
+            ))
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Peers {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            deserializer.deserialize_bytes(PeersVisitors)
+        }
+    }
+
+    impl Serialize for Peers {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let mut single_slice = Vec::with_capacity(6 * self.0.len());
+            for peer in &self.0 {
+                single_slice.extend(peer.ip().octets());
+                single_slice.extend(peer.port().to_ne_bytes());
+            }
+            serializer.serialize_bytes(&single_slice)
+        }
+    }
+}
